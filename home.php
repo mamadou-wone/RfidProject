41fd19125c4ec59378d220669d4b09f760dc9fea
Mamadou WONE <mamadouwone12345@gmail.com>
2020-05-17 14:32:54 +0000

    Add progress bar to the home page

--- home.php
+++ home.php
@@ -1,18 +1,68 @@
-<?php require 'header.php' ?>
-    <div class="container">
+<?php
+
+use App\DataBase;
+
+require 'header.php';
+$pdo = DataBase::getPDO('rfid_user')->prepare("SELECT  count(*) as count FROM donnees");
+$pdo->execute();
+$data =(int) $pdo->fetch()['count'];
+
+?>
+    <div class="container-fluid">
         <div class="row">
-          <h1 class="alert alert-success">WELCOME</h1>
-          <h1 class="alert alert-success">WELCOME</h1>
-          <h1 class="alert alert-success">WELCOME</h1>
-          <h1 class="alert alert-success">WELCOME</h1>
-          <h1 class="alert alert-success">WELCOME</h1>
-          <h1 class="alert alert-success">WELCOME</h1>
-          <h1 class="alert alert-success">WELCOME</h1>
+        <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card border-left-primary shadow h-100 py-2">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Nombre d'Etudiant</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $data?> </div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
         </div>
 
+        
     </div>
 
 
+    <div class="col-lg-9 mb-4">
+
+              <!-- Project Card Example -->
+              <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                  <h6 class="m-0 font-weight-bold text-primary">Projects</h6>
+                </div>
+                <div class="card-body">
+                  <h4 class="small font-weight-bold">Server Migration <span class="float-right">20%</span></h4>
+                  <div class="progress mb-4">
+                    <div class="progress-bar bg-danger" role="progressbar" style="width: 20%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
+                  </div>
+                  <h4 class="small font-weight-bold">Sales Tracking <span class="float-right">40%</span></h4>
+                  <div class="progress mb-4">
+                    <div class="progress-bar bg-warning" role="progressbar" style="width: 40%" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
+                  </div>
+                  <h4 class="small font-weight-bold">Customer Database <span class="float-right">60%</span></h4>
+                  <div class="progress mb-4">
+                    <div class="progress-bar" role="progressbar" style="width: 60%" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
+                  </div>
+                  <h4 class="small font-weight-bold">Payout Details <span class="float-right">80%</span></h4>
+                  <div class="progress mb-4">
+                    <div class="progress-bar bg-info" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+                  </div>
+                  <h4 class="small font-weight-bold">Account Setup <span class="float-right">Complete!</span></h4>
+                  <div class="progress">
+                    <div class="progress-bar bg-success" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                  </div>
+                </div>
+              </div>
+    </div>
+<br>
       <!-- Footer -->
       <footer class="sticky-footer bg-white">
         <div class="container my-auto">
